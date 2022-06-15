3c93cebb3662d2e329085d0fc80db433df615164
Nikita Ivanov <nikita.vyach.ivanov@gmail.com>
2022-06-15 07:24:59 +0500

    Minor

--- update.sh
+++ update.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+branch="$(git branch --show-current)"
+
+git stash
+git checkout master
+git pull
+git checkout "$branch"
+git stash pop
+
 git show master:doc/ctpv.1 | ./manhtml.sh > index.html
